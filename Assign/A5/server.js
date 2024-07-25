02aaf4d4900fb272908b1a5e96501c75321b93a2
maikerouxx <maikerouxx@gmail.com>
2024-07-25 19:00:50 -0400

    v5

--- Assign/A5/server.js
+++ Assign/A5/server.js
@@ -7,7 +7,7 @@ const collegeData = require('./modules/collegeData');
 const app = express();
 
 // Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'A5')));
+app.use('/data', express.static(path.join(__dirname, 'data')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'local')));
 // Configure Handlebars
